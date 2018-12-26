898c2c0c38b2fd042bc9ff5babcd2a4bced55e22
rajravichandrann <42481430+rajravichandrann@users.noreply.github.com>
2018-12-26 14:25:45 -0700

    Update cerebro_after_hours.py

--- cerebro_after_hours.py
+++ cerebro_after_hours.py
@@ -4,8 +4,10 @@ import os
 import requests
 #S3 resource
 s3 = boto3.client('s3')
-Bucket = "cerebro-bucket01"
-Key = "resources (1).csv"
+Bucket = event['Records'][0]['s3']['bucket']['name']
+Key  = event['Records'][0]['s3']['object']['key']
+#Bucket = "cerebro-bucket01"
+#Key = "resources (1).csv"
 raw_file = s3.get_object(Bucket=Bucket, Key=Key) #get the file from s3 bucket raw content
 #print(raw_file)
 file = raw_file['Body'].read().decode('utf-8').splitlines(True) #read the body of the required content
